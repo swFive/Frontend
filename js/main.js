0d83cf26da74af2dba56bf7fa80b053376f51f4d
Kim_SJ <107867776+k-dev178@users.noreply.github.com>
2025-10-02 15:49:48 +0900

    [add]모바일 헤더 구현

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { initSearchBar } from './components/search.js';
+import { initHeaderBarHide } from './components/header_bar.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	initSearchBar();
+	initHeaderBarHide();
+});
